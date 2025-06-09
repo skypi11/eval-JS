7d6ea639901f8f343900a8a9b4b5ebee0f7c7dd5
skypi11 <mattmolines@gmail.com>
2025-06-10 00:20:59 +0200

    Edit
    Edit js for order page but is not finish, only display localstorage

--- front/js/order.js
+++ front/js/order.js
@@ -1,1 +1,61 @@
-let panier = JSON.parse(localStorage.getItem("panier")) || []
+async function init() {
+    const produits = await getProducts();
+    afficherPanier(produits);
+}
+init();
+
+async function getProducts() {
+    try {
+        const req = await fetch("http://localhost:3000/api/products");
+        return await req.json();
+    } catch (error) {
+        console.error("erreur lors de la récupération des données");
+    }
+}
+
+let panier = JSON.parse(localStorage.getItem("panier")) || [];
+const panierArticle = document.querySelector(".panierArticle");
+const totalArticle = document.querySelector(".totalArticle");
+
+function afficherPanier(produits) {
+    let total = 0;
+    let quantite = 0;
+    panierArticle.innerHTML = "";
+
+    for (const article of panier) {
+        const produitPanier = produits.find((p) => p._id === article.id);
+        const declinaison = produitPanier.declinaisons.find(
+            (d) => d.taille === article.taille
+        );
+        const prix = declinaison ? declinaison.prix : 0;
+        prixQuantite = prix * article.quantite;
+        total += prixQuantite;
+        quantite += article.quantite;
+
+        panierArticle.innerHTML += `
+        <img src="${article.image}" alt="${article.titre}" />
+                <p>${article.titre}</p>
+                <p>${article.taille}</p>
+                <p>${prixQuantite.toFixed(2)} €</p>
+                <div>
+                    <label for="quantity">Quantité : </label>
+                    <input
+                        type="number"
+                        name="quantity"
+                        id="quantity"
+                        placeholder="1"
+                        value="${article.quantite}"
+                        min="1"
+                        max="100"
+                    />
+                </div>
+                <a href="#" class= boutonSupprimer>supprimer</a>
+        `;
+    }
+    totalArticle.innerHTML = `
+        <p><strong>Total de la commande : ${total.toFixed(2)} €</strong></p>
+        <p>${quantite} articles pour un montant de ${total.toFixed(2)} €</p>
+    `;
+
+
+}
--- front/js/product.js
+++ front/js/product.js
@@ -17,101 +17,130 @@ async function getProduct(id) {
 }
 
 function oneProduct(produit) {
-    const ligne = produit.description.split(". ")
-    const extrait = ligne.slice(0,2).join(". ")
+    const ligne = produit.description.split(". ");
+    const extrait = ligne.slice(0, 2).join(". ");
     const template = `<article>
-                <figure>
-                    <img src="${produit.image}" alt="Titre de l'oeuvre" />
-                </figure>
-                <div>
-                    <h1>${produit.titre}</h1>
-                    <p>${extrait}
-                    </p>
-                    <div class="price">
-                        <p>Acheter pour</p>
-                        <span class="showprice"></span>
-                    </div>
-                    <div class="declinaison">
-                        <input
-                            type="number"
-                            name="quantity"
-                            id="quantity"
-                            placeholder="1"
-                            value="1"
-                            min="1"
-                            max="100"
-                        />
-                        <select name="format" id="format"></select>
-                    </div>
-                    <a class="button-buy" href="#">Buy ${produit.shorttitle}</a>
-                </div>
-            </article>
-
-            <aside>
-                <h2>Description de l'oeuvre : ${produit.titre}</h2>
-                <p>${produit.description}</p>
-            </aside>
-  `;
+                            <figure>
+                                <img src="${produit.image}" alt="${produit.titre}" />
+                            </figure>
+                                <div>
+                                    <h1>${produit.titre}</h1>
+                                    <p>${extrait}
+                                    </p>
+                                    <div class="price">
+                                        <p>Acheter pour</p>
+                                        <span class="showprice"></span>
+                                    </div>
+                                    <div class="declinaison">
+                                        <input
+                                            type="number"
+                                            name="quantity"
+                                            id="quantity"
+                                            placeholder="1"
+                                            value="1"
+                                            min="1"
+                                            max="100"
+                                        />
+                                        <select name="format" id="format"></select>
+                                    </div>
+                                        <button class="button-buy">Buy ${produit.shorttitle}</button>
+                                        <div id="ajoutPanier">
+                                            Produit ajouté au panier !
+                                        </div>
+                                    </div>
+                            </article>
+                            <aside>
+                                <h2>Description de l'oeuvre : ${produit.titre}</h2>
+                                <p>${produit.description}</p>
+                            </aside>
+                `;
     const section = document.querySelector(".detailoeuvre");
     section.insertAdjacentHTML("beforeend", template);
 
     const select = document.querySelector("#format");
-    produit.declinaisons.forEach((element, index) => {
+    produit.declinaisons.forEach((element) => {
         const option = document.createElement("option");
         // console.log(element,index)
-        option.value = index;
-        option.textContent = `${element.taille}`;
+        option.value = element.taille;
+        option.textContent = element.taille;
         select.appendChild(option);
     });
     const quantite = document.querySelector("#quantity");
     const prix = document.querySelector(".showprice");
 
     function prixTotal() {
-        const tailleIndex = select.value;
-        const declinaison = produit.declinaisons[tailleIndex];
+        const tailleChoisie = select.value;
+        const declinaison = produit.declinaisons.find(
+            (d) => d.taille === tailleChoisie
+        );
         let nbQuantite = parseInt(quantite.value) || 1;
 
-        if (nbQuantite<1) {
-            nbQuantite=1
+        if (nbQuantite < 1) {
+            nbQuantite = 1;
         }
-        if (nbQuantite>100) {
-            nbQuantite=100
+        if (nbQuantite > 100) {
+            nbQuantite = 100;
         }
 
         const total = declinaison.prix * nbQuantite;
         prix.textContent = `${total.toFixed(2)}€`;
     }
 
-    select.addEventListener("change", prixTotal);
-    quantite.addEventListener("input", prixTotal);
-    prixTotal();
-
-    const button = document.querySelector(".button-buy")
-    button.addEventListener("click",(e)=>{
-        e.preventDefault()
-        const declinaisonIndex = document.querySelector("#format").value
-        const quantite = parseInt(document.querySelector("#quantity").value) || 1
-
-        ajoutPanier(produit, declinaisonIndex, quantite)
-
-    })
-}
-function ajoutPanier(produit, declinaisonIndex, quantite) {
-    let panier = JSON.parse(localStorage.getItem("panier")) || []
-    const idProduit = produit._id + "-" + declinaisonIndex
-    const existant = panier.find(item => item.idProduit === idProduit)
-    
-    if(existant){
-        existant.quantite += quantite
-    }else{
-        panier.push({
-            idProduit : idProduit,
-            id : produit._id,
-            image : produit.image,
-            titre : produit.titre,
-            format : produit.declinaisons[declinaisonIndex].taille,
-            quantite : quantite
-        })
+    const button = document.querySelector(".button-buy");
+    function checkQuantite() {
+        let nbQuantite = parseInt(quantite.value) || 1;
+        const valide = nbQuantite >= 1 && nbQuantite <= 100;
+        button.disabled = !valide;
+        if (!valide) {
+            prix.innerHTML = `<span style="color: red;">La quantité doit etre comprise <br> entre 1 et 100</span>`;
+        } else {
+            prixTotal();
+        }
     }
-localStorage.setItem("panier", JSON.stringify(panier))
+    select.addEventListener("change", prixTotal);
+    //quantite.addEventListener("input", prixTotal);
+    quantite.addEventListener("input", checkQuantite);
+    //prixTotal();
+    checkQuantite();
+
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (button.disabled) {
+            return;
+        }
+        const tailleChoisie = select.value;
+        const quantite =
+            parseInt(document.querySelector("#quantity").value) || 1;
+
+        ajoutPanier(produit, tailleChoisie, quantite);
+        confirmAjoutPanier();
+    });
+}
+function confirmAjoutPanier() {
+    const toast = document.getElementById("ajoutPanier");
+    toast.style.display = "block";
+    setTimeout(() => {
+        toast.style.display = "none";
+    }, 2000); // 2 secondes
+}
+function ajoutPanier(produit, tailleChoisie, quantite) {
+    let panier = JSON.parse(localStorage.getItem("panier")) || [];
+    const declinaison = produit.declinaisons.find(
+        (d) => d.taille === tailleChoisie
+    );
+    const idProduit = produit._id + "-" + declinaison.taille;
+    const existant = panier.find((item) => item.idProduit === idProduit);
+    if (existant) {
+        existant.quantite += quantite;
+    } else {
+        panier.push({
+            idProduit: idProduit,
+            id: produit._id,
+            image: produit.image,
+            titre: produit.titre,
+            taille: declinaison.taille,
+            quantite: quantite,
+        });
+    }
+    localStorage.setItem("panier", JSON.stringify(panier));
 }
